a654cb0c16d3bde7caa51a8ff2bf92c2b731bb67
FidelFuentes <fidelfuentes.fullstack@gmail.com>
2021-10-25 11:04:55 -0300

    final del curso

---
+++ index.js
@@ -0,0 +1,66 @@
+// html semantico
+
+// no uses muchos div=> nos obliga a usar las etiquetas adecuadas para cada parte del html
+//  les da sentido a la estructura
+
+// las etiquetas mas usadas
+
+// div => layout (header,nav, section, article, aside, footer)
+// enlaces => a
+// textos => h1..h6, p , span(letra negrita o subrayada)
+//imagenes y videos => img, svg iframe, video
+// formularios => form: estructura inicial, input , label : texto inicial (titulo de input), button
+//listas ul , li, ol
+
+/* selectores de css
+selectores basicos
+de tipo => div
+de clase => . elemento{}
+de id=> #id
+de atributo => a[href='']{}
+universal => *
+
+slectores combinadores
+
+desendientes div p
+hijo directo div > p
+elementos adyacentes div + p
+
+general de hermanos div~p
+
+
+pseudo clases  => nos permite llegar a las acciones que realiza el usario 
+:active
+:focus
+:hover
+:nth-child(n)
+
+pseudoelementos => nos permiten acceder a elementos de html que no son accesibles
+
+:: after
+:: before
+::first-letter
+::placeholder
+
+css => cascading style sheet  hojas de estilo en cascada
+
+
+
+orden de especificidad
+
+!important
+estilos en linea
+id
+clases , atributos ,pseudoclases
+elementos y pseudoelementos
+selecto universal
+
+
+
+display : es el tipo de visualizacion que tienen los elementos
+block
+inline  => no acepta width y height pero si margin
+inline-block => su visualizacion es de tipo inline pero tiene caracteristicas de tipo bloque
+flex
+grid
+*/
